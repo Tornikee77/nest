fcfa6b443d0f22a50afeef77229de8333abb20e2
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-12-07 23:46:53 +0400

    Add Swagger API documentation setup
    Integrated @nestjs/swagger for API documentation. Updated main.ts to configure and serve Swagger UI at /api. Removed unused default controller and service methods.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,21 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // create the swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs blog API')
+    .setDescription('use the base API URL as: http://localhost:3000')
+    .setTermsOfService('http://localhost:3000/terms-of-service')
+    .setLicense('MIT', 'http://localhost:3000')
+    .addServer('http://localhost:3000/')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
