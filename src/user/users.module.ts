5334630cb897b2e79feab939e76a5b8f49e9d8c2
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-12-10 02:27:21 +0400

    Add User entity and integrate TypeORM
    Introduced a new User entity in the user module and updated users.module.ts to support it. Integrated TypeORM and PostgreSQL by adding relevant dependencies and updating app.module.ts. Also updated .gitignore to exclude Compodoc documentation output.

--- src/user/users.module.ts
+++ src/user/users.module.ts
@@ -2,11 +2,14 @@ import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsersService } from 'src/user/providers/users.service';
+import { Type } from 'class-transformer';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
