08ff2f00b852aeeaae9e98315647cbd9cd7042ca
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-02-02 16:24:39 +0400

    Update posts.service.ts

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,26 +1,74 @@
+import { CreatePostDto } from '../dtos/create-post.dto';
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/user/providers/users.service';
+import { MetaOptionsService } from './../../meta-options/meta-options.service';
+import { UsersService } from 'src/users/providers/users.service';
+import { Repository } from 'typeorm';
+import { Post } from '../post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MetaOption } from 'src/meta-options/meta-option.entity';
 
 @Injectable()
 export class PostsService {
-  // Injecting UsersServices
-  constructor(private readonly usersService: UsersService) {}
-  //                1234
-  public findAll(userId: string) {
-    const user = this.usersService.findOneById(userId);
-    // Users Service
-    // find a User
-    return [
-      {
-        user: user,
-        title: 'First Post',
-        content: 'First content',
+  constructor(
+    /*
+     * Injecting Users Service
+     */
+    private readonly usersService: UsersService,
+
+    /**
+     * Injecting postsRepository
+     */
+    @InjectRepository(Post)
+    private readonly postsRepository: Repository<Post>,
+
+    /**
+     * Inject metaOptionsRepository
+     */
+    @InjectRepository(MetaOption)
+    private readonly metaOptionsRepository: Repository<MetaOption>,
+  ) {}
+
+  /**
+   * Method to create a new post
+   */
+  public async create(createPostDto: CreatePostDto) {
+    // Create the post
+    // !old
+    // let post = this.postsRepository.create({
+    //   ...createPostDto,
+    // });
+
+    let post = this.postsRepository.create({
+      ...createPostDto,
+      metaOptions: createPostDto.metaOptions
+        ? this.metaOptionsRepository.create(createPostDto.metaOptions)
+        : undefined,
+    });
+
+    return await this.postsRepository.save(post);
+  }
+
+  /**
+   * Method to find all posts
+   */
+  public async findAll(userId: string) {
+    // find all posts
+    let posts = await this.postsRepository.find({
+      relations: {
+        metaOptions: true,
       },
-      {
-        user: user,
-        title: 'Second Post',
-        content: 'second content',
-      },
-    ];
+    });
+
+    return posts;
+  }
+
+  /**
+   * Method to delete a post from the database
+   */
+  public async delete(id: number) {
+    // Find the post from the database
+    await this.postsRepository.delete(id);
+
+    return { deleted: true, id };
   }
 }
