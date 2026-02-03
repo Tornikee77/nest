fcfac2287593265407ebdab990e0dc6fbfa52df0
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-02-03 18:26:36 +0400

    bugfix/post-module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,14 +1,19 @@
+// posts.module.ts
 import { Module } from '@nestjs/common';
-
 import { PostsController } from './posts.controller';
 import { PostsService } from './providers/posts.service';
 import { UsersModule } from 'src/user/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './post.entity';
+import { MetaOption } from 'src/meta-options/meta-option.entity'; // Import the entity
 
 @Module({
   controllers: [PostsController],
   providers: [PostsService],
-  imports: [UsersModule,TypeOrmModule.forFeature([Post])],
+  imports: [
+    UsersModule,
+    // Add MetaOption here!
+    TypeOrmModule.forFeature([Post, MetaOption]),
+  ],
 })
 export class PostsModule {}
--- src/user/providers/users.service.ts
+++ src/user/providers/users.service.ts
@@ -1,14 +1,33 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { AuthService } from 'src/auth/providers/auth.service';
 import { GetUserParamDto } from 'src/users/dtos/get-users-param.dto';
+import { User } from '../user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
     // Injecting Auth Service
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
+
+  public async createUser(createUserDto: CreateUserDto) {
+    let existingUser: User | null = null;
+    existingUser = await this.userRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    let newUser = this.userRepository.create(createUserDto);
+    newUser = await this.userRepository.save(newUser);
+
+    return newUser;
+  }
 
   // Method to find all Users
   public findAll(
