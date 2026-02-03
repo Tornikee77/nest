5db3d73bce2b09ee9d5081a20d53d115f3a90c98
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-02-03 20:53:51 +0400

    Update users.service.ts

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,16 +1,17 @@
-import { GetUsersParamDto } from '../dtos/get-users-param.dto';
+import { GetUsersParamDto } from "../dtos/get-users-param.dto";
 import {
   BadRequestException,
   Inject,
   Injectable,
   InternalServerErrorException,
   forwardRef,
-} from '@nestjs/common';
-import { User } from '../user.entity';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
-import { CreateUserDto } from '../dtos/create-user.dto';
-import { AuthService } from 'src/auth/providers/auth.service';
+} from "@nestjs/common";
+import { User } from "../user.entity";
+import { DataSource, Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CreateUserDto } from "../dtos/create-user.dto";
+import { AuthService } from "src/auth/providers/auth.service";
+import { create } from "domain";
 
 /**
  * Controller class for '/users' API endpoint
@@ -27,6 +28,9 @@ export class UsersService {
     // Injecting Auth Service
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
+
+    //Injecting DataSource
+    private readonly dataSource: DataSource,
   ) {}
 
   public async createUser(createUserDto: CreateUserDto) {
@@ -48,6 +52,34 @@ export class UsersService {
     return newUser;
   }
 
+  public async createmany(createUserDtos: CreateUserDto[]) {
+    let newUsers: User[] = [];
+    // create a query runner
+    const queryRunner = this.dataSource.createQueryRunner();
+    // connect the query runner to database
+    await queryRunner.connect();
+    // start a transaction
+    await queryRunner.startTransaction();
+
+    try {
+      // createUserDtos
+      for (const user of createUserDtos) {
+        let newUser = queryRunner.manager.create(User, user);
+        let result = await queryRunner.manager.save(newUser);
+        newUsers.push(result);
+      }
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      // if we have error, rollback the transaction
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException(
+        "Unable to create users at this time, please try again",
+      );
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
   /**
    * Public method responsible for handling GET request for '/users' endpoint
    */
@@ -58,12 +90,12 @@ export class UsersService {
   ) {
     return [
       {
-        firstName: 'John',
-        email: 'john@doe.com',
+        firstName: "John",
+        email: "john@doe.com",
       },
       {
-        firstName: 'Alice',
-        email: 'alice@doe.com',
+        firstName: "Alice",
+        email: "alice@doe.com",
       },
     ];
   }
@@ -74,8 +106,8 @@ export class UsersService {
   public findOneById(id: string) {
     return {
       id: 1234,
-      firstName: 'Alice',
-      email: 'alice@doe.com',
+      firstName: "Alice",
+      email: "alice@doe.com",
     };
   }
 }
