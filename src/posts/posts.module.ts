ee6ac6046a72c5415f0f00be76741f779c14c493
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-02-05 01:29:15 +0400

    feature/pagination dto

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,19 +1,17 @@
-import { MetaOption } from 'src/meta-options/meta-option.entity';
-import { MetaOptionsModule } from 'src/meta-options/meta-options.module';
-import { Module } from '@nestjs/common';
-import { Post } from './post.entity';
-import { PostsController } from './posts.controller';
-import { PostsService } from './providers/posts.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from 'src/users/users.module';
+import { MetaOption } from "src/meta-options/meta-option.entity";
+import { MetaOptionsModule } from "src/meta-options/meta-options.module";
+import { Module } from "@nestjs/common";
+import { Post } from "./post.entity";
+import { PostsController } from "./posts.controller";
+import { PostsService } from "./providers/posts.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "src/users/users.module";
+import { User } from "src/users/user.entity";
+import { TagsModule } from "src/tags/tags.module";
 
 @Module({
   controllers: [PostsController],
   providers: [PostsService],
-  imports: [
-    UsersModule,
-    TypeOrmModule.forFeature([Post, MetaOption]),
-    MetaOptionsModule,
-  ],
+  imports: [UsersModule, TypeOrmModule.forFeature([Post, User]), TagsModule],
 })
 export class PostsModule {}
--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
-import { Tag } from './tag.entity';
-import { TagsController } from './tags.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { Tag } from "./tag.entity";
+import { TagsController } from "./tags.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   controllers: [TagsController],
