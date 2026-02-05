1bd8ebd387318e45cb6d3b823ababa10ddbbeeb5
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-02-05 20:32:00 +0400

    feature/bcrypt and hashing password skeletons

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,20 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module, forwardRef } from "@nestjs/common";
 
-import { AuthController } from './auth.controller';
-import { AuthService } from './providers/auth.service';
-import { UsersModule } from 'src/users/users.module';
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./providers/auth.service";
+import { UsersModule } from "src/users/users.module";
+import { HashingProvider } from "./providers/hashing.provider";
+import { BcryptProvider } from "./providers/bcrypt.provider";
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: HashingProvider,
+      useClass: BcryptProvider,
+    },
+  ],
   imports: [forwardRef(() => UsersModule)],
   exports: [AuthService],
 })
--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -1,6 +1,6 @@
-import { Inject, Injectable, forwardRef } from '@nestjs/common';
+import { Inject, Injectable, forwardRef } from "@nestjs/common";
 
-import { UsersService } from 'src/users/providers/users.service';
+import { UsersService } from "src/users/providers/users.service";
 
 @Injectable()
 export class AuthService {
@@ -9,14 +9,4 @@ export class AuthService {
     @Inject(forwardRef(() => UsersService))
     private readonly usersService: UsersService,
   ) {}
-
-  public login(email: string, password: string, id: string) {
-    const user = this.usersService.findOneById('1234');
-    // login
-    return 'SAMPLE_TOKEN';
-  }
-
-  public isAuth() {
-    return true;
-  }
 }
