896a348d2702aea34ee943827e5f55485003cb52
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-02-05 20:40:02 +0400

    feature/signin dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { AuthService } from './providers/auth.service';
-import { Controller } from '@nestjs/common';
+import { SignInDto } from "./dtos/sign-in.dto";
+import { AuthService } from "./providers/auth.service";
+import { Body, Controller, Post } from "@nestjs/common";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   constructor(
     /*
@@ -9,4 +10,7 @@ export class AuthController {
      */
     private readonly authService: AuthService,
   ) {}
+
+  @Post()
+  public async signIn(@Body() signInDto: SignInDto) {}
 }
--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable, forwardRef } from "@nestjs/common";
 
 import { UsersService } from "src/users/providers/users.service";
+import { SignInDto } from "../dtos/sign-in.dto";
 
 @Injectable()
 export class AuthService {
@@ -9,4 +10,11 @@ export class AuthService {
     @Inject(forwardRef(() => UsersService))
     private readonly usersService: UsersService,
   ) {}
+
+  public signIn(signInDto: SignInDto) {
+    // find user be email Id
+    // thro exception if not found
+    // compare the password to hash
+    // send confirmation
+  }
 }
