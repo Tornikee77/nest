7cf3d64d13e219439e867562886abcde6710bce5
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-02-07 23:56:41 +0400

    feature/updated user-service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,39 @@
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { MetaOptionsModule } from './meta-options/meta-options.module';
-import { Module } from '@nestjs/common';
-import { PostsModule } from './posts/posts.module';
-import { Tag } from './tags/tag.entity';
-import { TagsModule } from './tags/tags.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { MetaOptionsModule } from "./meta-options/meta-options.module";
+import { Module } from "@nestjs/common";
+import { PostsModule } from "./posts/posts.module";
+import { Tag } from "./tags/tag.entity";
+import { TagsModule } from "./tags/tags.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 /**
  * Importing Entities
  * */
-import { User } from './users/user.entity';
-import { UsersModule } from './users/users.module';
-import { PaginationModule } from './common/pagination/pagination.module';
+import { User } from "./users/user.entity";
+import { UsersModule } from "./users/users.module";
+import { PaginationModule } from "./common/pagination/pagination.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
     UsersModule,
     PostsModule,
     AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [],
       inject: [],
       useFactory: () => ({
-        type: 'postgres',
+        type: "postgres",
         // entities: [User],
         synchronize: true,
         port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        host: 'localhost',
+        username: "postgres",
+        password: "postgres",
+        host: "localhost",
         autoLoadEntities: true,
-        database: 'nestblog-api',
+        database: "nestblog-api",
       }),
     }),
     TagsModule,
